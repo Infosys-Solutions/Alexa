e4b79a590bb68215a6d4f45ddec9991e82667aad
rizwan atta <rizwanatta890@gmail.com>
2017-03-20 17:56:55 +0500

    This is firs global Variable in E6 test

---
+++ global.js
@@ -0,0 +1,18 @@
+// var abc = "This is rizwan atta";
+// var NodeVariable = abc.slice(7);
+// console.log(`THis is mine case of Life talking about ${NodeVariable}`);
+
+// // to show the directory name in which we are located I use this!.. thing .. here!... case
+
+// console.log(__dirname);
+
+
+// the Path module is present in global location so we are using it as our Path plucker
+
+// using require function available in globle we bring other modules in our module!
+
+// for example I am making this Module here for Mine Own Case .. Lets say we want to get name of the file I am currently working
+// in for node JS  I will do something like this
+
+var Path = require("path"); //here the thing "path" is basically the path module presentin our global 
+console.log(`your File name is ${Path.basename(__filename)}`);
